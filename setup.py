390a53f950aa347d77fd99ada76eb1d3bc111222
tsurubee <29603512+tsurubee@users.noreply.github.com>
2021-06-27 10:15:33 +0900

    Add black to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages
 
+DEV_REQUIRES = ["black"]
+
 setup(
     name='shappack',
     version='0.1.0',
@@ -8,5 +10,8 @@ setup(
     packages=find_packages(exclude=('tests', 'docs')),
     install_requires=[
         'numpy',
-    ]
+    ],
+    extras_require={
+        "dev": DEV_REQUIRES,
+    },
 )
