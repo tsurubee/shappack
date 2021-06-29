a9de769c8ac6372e633db517393b66ddaecb88f2
tsurubee <29603512+tsurubee@users.noreply.github.com>
2021-06-29 09:19:05 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
 from setuptools import setup, find_packages
 
-DEV_REQUIRES = ["black"]
+DEV_REQUIRES = ["black", "shap"]
 
 setup(
-    name='shappack',
-    version='0.1.0',
-    #description='',
-    author='Hirofumi Tsuruta',
-    packages=find_packages(exclude=('tests', 'docs')),
-    install_requires=[
-        'numpy',
-    ],
+    name="shappack",
+    version="0.1.0",
+    # description='',
+    author="Hirofumi Tsuruta",
+    packages=find_packages(exclude=("tests", "docs")),
+    install_requires=["numpy", "pandas", "sklearn", "scipy"],
     extras_require={
         "dev": DEV_REQUIRES,
     },
