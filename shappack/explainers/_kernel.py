Fix computation of base value

shappack/explainers/_kernel.py
@@ -9,7 +9,7 @@ class KernelExplainer(BaseExplainer):
         self.model = model
         self.data = self.convert_to_nparray(data)
         self.link = convert_to_link(link)
-        self.linkfv = np.vectorize(self.link.f)
+        self.linkf = np.vectorize(self.link.f)
         self.n_data = self.data.shape[0]
         self.n_features = self.data.shape[1]
 
@@ -23,7 +23,8 @@ class KernelExplainer(BaseExplainer):
 
         # Compute base value (=E[f(x)]=phi_0)
         out_val = self.model(self.data)
-        self.base_val = self.linkfv(np.squeeze(out_val / self.n_data))
+        self.base_val = np.sum(self.linkf(out_val)) / self.n_data
+        # TODO: Support classification problem outputs
 
     def shap_values(self, X, n_samples="auto"):
         pass