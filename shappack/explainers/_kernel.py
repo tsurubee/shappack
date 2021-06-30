d92fd1673a5f4b3d9a6cf2e0dcdfd765205dc8fb
tsurubee <29603512+tsurubee@users.noreply.github.com>
2021-06-30 11:00:00 +0900

    Add the process of sampling subsets

--- shappack/explainers/_kernel.py
+++ shappack/explainers/_kernel.py
@@ -1,5 +1,8 @@
 import warnings
+import copy
+import itertools
 import numpy as np
+from scipy.special import binom
 from ._base import BaseExplainer
 from ..utils._link import convert_to_link
 
@@ -34,7 +37,7 @@ class KernelExplainer(BaseExplainer):
                 raise ValueError(
                     "The number of features in instance X and the background dataset do not match."
                 )
-            shap_values = self._shap_values(instance, n_samples)
+            shap_values = self._shap_values(instance, n_samples, l1_reg)
             return shap_values
         elif len(X.shape) == 2:
             # TODO: Support multiple instances
@@ -43,15 +46,74 @@ class KernelExplainer(BaseExplainer):
             raise ValueError("The instances X to be interpreted must be a vector or 2D matrix")
 
     def _shap_values(self, instance, n_samples, l1_reg):
+        # Compute f(x), the predicted value for instance
+        self.fx = self.model(instance)
+        # If there is only one feature, it has all the effects
+        if self.n_features == 1:
+            phi = self.link.f(self.fx) - self.link.f(self.base_val)
+            return phi
+
         # Sampling Subsets (Set of binary vectors)
+        self._sampling(n_samples)
 
         # Applying the Characteristic Function
 
         # Solving Weighted Least Squares
-        pass
+        return
 
     def _sampling(self, n_samples):
-        pass
+        if n_samples == "auto":
+            self.n_samples = 2 * self.n_features + 2 ** 11
+        else:
+            self.n_samples = n_samples
+        self.subsets = np.zeros((self.n_samples, self.n_features))
+        self.kernel_weights = np.zeros(self.n_samples)
+        self.n_added_samples = 0
+        n_subset_size = np.int(np.ceil((self.n_features - 1) / 2.0))
+        n_paired_subset_size = np.int(np.floor((self.n_features - 1) / 2.0))
+        weight_vector = np.array(
+            [
+                (self.n_features - 1.0) / (i * (self.n_features - i))
+                for i in range(1, n_subset_size + 1)
+            ]
+        )
+        weight_vector[:n_paired_subset_size] *= 2
+        weight_vector /= np.sum(weight_vector)
+        remaining_weight_vector = copy.copy(weight_vector)
+        n_remaining_samples = self.n_samples
+        n_full_subsets = 0
+        binary_vec = np.zeros(self.n_features)
+        for subset_size in range(1, n_subset_size + 1):
+            n_sampling_subsets = binom(self.n_features, subset_size)
+            if subset_size <= n_paired_subset_size:
+                n_sampling_subsets *= 2
+            # If there are enough number of remaining samples to sample n_sampling_subsets
+            if n_remaining_samples * remaining_weight_vector[subset_size - 1] > n_sampling_subsets:
+                n_full_subsets += 1
+                n_remaining_samples -= n_sampling_subsets
+                if remaining_weight_vector[subset_size - 1] < 1.0:
+                    remaining_weight_vector /= 1 - remaining_weight_vector[subset_size - 1]
+                weight = weight_vector[subset_size - 1] / binom(self.n_features, subset_size)
+                if subset_size <= n_paired_subset_size:
+                    weight /= 2.0
+                for inds in itertools.combinations(
+                    np.arange(self.n_features, dtype="int64"), subset_size
+                ):
+                    binary_vec[:] = 0.0
+                    binary_vec[np.array(inds, dtype="int64")] = 1.0
+                    self._add_sample(binary_vec, weight)
+                    # Create a binary vector with inverted "0" and "1".
+                    if subset_size <= n_paired_subset_size:
+                        binary_vec[:] = np.abs(binary_vec - 1)
+                        self._add_sample(binary_vec, weight)
+            else:
+                break
+        # TODO: Add samples to the rest of the subset space
+
+    def _add_sample(self, binary_vec, weight):
+        self.subsets[self.n_added_samples, :] = binary_vec
+        self.kernel_weights[self.n_added_samples] = weight
+        self.n_added_samples += 1
 
     def _solve(self):
         pass
