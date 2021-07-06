4faac58bae59f32ff1b9d2db3be571119519bb8e
tsurubee <29603512+tsurubee@users.noreply.github.com>
2021-07-06 14:32:10 +0900

    Support classification problem outputs (characteristic_func)

--- shappack/characteristic_funcs/reference.py
+++ shappack/characteristic_funcs/reference.py
@@ -11,7 +11,13 @@ def kernel_shap(instance, subsets, model, data):
         synth_data[offset : offset + n_data, features_idx] = instance[:, features_idx][0]
     model_preds = model(synth_data)
     # Computing the expected value of the model's predictions for the background dataset
-    ey = np.zeros(n_subsets)
-    for i in range(n_subsets):
-        ey[i] = np.mean(model_preds[i * n_data : i * n_data + n_data])
+    if len(model_preds.shape) == 1:
+        ey = np.zeros(n_subsets)
+        for i in range(n_subsets):
+            ey[i] = np.mean(model_preds[i * n_data : i * n_data + n_data])
+    else:
+        out_dim = model_preds.shape[1]
+        ey = np.zeros((n_subsets, out_dim))
+        for i in range(n_subsets):
+            ey[i, :] = np.mean(model_preds[i * n_data : i * n_data + n_data, :], 0)
     return ey
