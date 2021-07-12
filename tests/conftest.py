0e368548ca257473acde083fc41808093062c409
tsurubee <29603512+tsurubee@users.noreply.github.com>
2021-07-12 16:21:21 +0900

    Add tests for Kernel SHAP

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,12 @@
 import pytest
-import numpy as np
-from sklearn.datasets import load_boston
+from sklearn.datasets import load_boston, load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.svm import SVR
+from sklearn.svm import SVR, SVC
 
-SEED = 123
 
 @pytest.fixture()
 def boston_data():
-    np.random.seed(SEED)
     boston = load_boston()
     X_train, X_test, y_train, _ = train_test_split(boston["data"], boston["target"], test_size=0.2)
     scaler = StandardScaler()
@@ -18,3 +15,12 @@ def boston_data():
     model = SVR()
     model.fit(X_train, y_train)
     return (X_train, X_test, y_train, model)
+
+@pytest.fixture()
+def iris_data():
+    iris = load_iris()
+    X_train, X_test, y_train, _ = train_test_split(iris["data"], iris["target"], test_size=0.2)
+    model = SVC(kernel="rbf", probability=True)
+    model.fit(X_train, y_train)
+    return (X_train, X_test, y_train, model)
+
