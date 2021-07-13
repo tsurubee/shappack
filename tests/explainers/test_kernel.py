e285682d56f060cc4f8c90747ddaf5387a52835d
tsurubee <29603512+tsurubee@users.noreply.github.com>
2021-07-14 08:51:01 +0900

    Fix background dataset size for test

--- tests/explainers/test_kernel.py
+++ tests/explainers/test_kernel.py
@@ -2,12 +2,14 @@ import shappack
 import numpy as np
 from typing import Tuple
 
+DATA_SIZE = 10
+
 
 def test_shap_values_regression(
     boston_data: Tuple[np.ndarray, np.ndarray, np.ndarray, "sklearn.svm.SVR"]
 ) -> None:
     X_train, X_test, y_train, model = boston_data
-    explainer = shappack.KernelExplainer(model.predict, X_train[0:10])
+    explainer = shappack.KernelExplainer(model.predict, X_train[:DATA_SIZE])
     shap_value = explainer.shap_values(X_test[1])
     assert len(shap_value) == X_train.shape[1]
     assert np.round(np.sum(shap_value), 2) == np.round(explainer.fx - explainer.base_val, 2)
@@ -17,7 +19,7 @@ def test_shap_values_classification(
     iris_data: Tuple[np.ndarray, np.ndarray, np.ndarray, "sklearn.svm.SVC"]
 ) -> None:
     X_train, X_test, y_train, model = iris_data
-    explainer = shappack.KernelExplainer(model.predict_proba, X_train, link="logit")
+    explainer = shappack.KernelExplainer(model.predict_proba, X_train[:DATA_SIZE], link="logit")
     shap_value = explainer.shap_values(X_test[0], n_samples=100)
     assert shap_value.shape[0] == explainer.out_dim
     assert shap_value.shape[1] == X_train.shape[1]
@@ -27,7 +29,7 @@ def test_shap_values_multiprocessing(
     boston_data: Tuple[np.ndarray, np.ndarray, np.ndarray, "sklearn.svm.SVR"]
 ) -> None:
     X_train, X_test, y_train, model = boston_data
-    explainer = shappack.KernelExplainer(model.predict, X_train[0:10])
+    explainer = shappack.KernelExplainer(model.predict, X_train[:DATA_SIZE])
     shap_value = explainer.shap_values(X_test[15], n_workers=-1)
     assert len(shap_value) == X_train.shape[1]
     assert np.round(np.sum(shap_value), 2) == np.round(explainer.fx - explainer.base_val, 2)
@@ -37,7 +39,7 @@ def test_shap_values_own_characteristic_func(
     boston_data: Tuple[np.ndarray, np.ndarray, np.ndarray, "sklearn.svm.SVR"]
 ) -> None:
     X_train, X_test, y_train, model = boston_data
-    explainer = shappack.KernelExplainer(model.predict, X_train[0:10])
+    explainer = shappack.KernelExplainer(model.predict, X_train[:DATA_SIZE])
 
     def my_characteristic_func(instance, subsets, model, data):
         return np.ones(subsets.shape[0])
@@ -50,7 +52,7 @@ def test_shap_values_skip_features(
     boston_data: Tuple[np.ndarray, np.ndarray, np.ndarray, "sklearn.svm.SVR"]
 ) -> None:
     X_train, X_test, y_train, model = boston_data
-    explainer = shappack.KernelExplainer(model.predict, X_train[0:10])
+    explainer = shappack.KernelExplainer(model.predict, X_train[:DATA_SIZE])
     shap_value = explainer.shap_values(X_test[1], skip_features=[0, 1])
     assert len(shap_value) == X_train.shape[1] - explainer.n_skip_features
     assert np.round(np.sum(shap_value), 2) == np.round(explainer.fx - explainer.base_val, 2)
