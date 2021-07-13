dd5d1b31d376fd000a72992e445ea2583b38787a
tsurubee <29603512+tsurubee@users.noreply.github.com>
2021-07-13 16:50:38 +0900

    Add test case of skip features

--- tests/explainers/test_kernel.py
+++ tests/explainers/test_kernel.py
@@ -44,3 +44,13 @@ def test_shap_values_own_characteristic_func(
 
     shap_value = explainer.shap_values(X_test[1], characteristic_func=my_characteristic_func)
     assert len(shap_value) == X_train.shape[1]
+
+
+def test_shap_values_skip_features(
+    boston_data: Tuple[np.ndarray, np.ndarray, np.ndarray, "sklearn.svm.SVR"]
+) -> None:
+    X_train, X_test, y_train, model = boston_data
+    explainer = shappack.KernelExplainer(model.predict, X_train[0:10])
+    shap_value = explainer.shap_values(X_test[1], skip_features=[0, 1])
+    assert len(shap_value) == X_train.shape[1] - explainer.n_skip_features
+    assert np.round(np.sum(shap_value), 2) == np.round(explainer.fx - explainer.base_val, 2)
