d7cb46d4bbb9e011dcd73ac5b4f521d231dcafeb
tsurubee <29603512+tsurubee@users.noreply.github.com>
2021-07-14 17:56:24 +0900

    Add test case of multiple instances

--- tests/explainers/test_kernel.py
+++ tests/explainers/test_kernel.py
@@ -56,3 +56,14 @@ def test_shap_values_skip_features(
     shap_value = explainer.shap_values(X_test[1], skip_features=[0, 1])
     assert len(shap_value) == X_train.shape[1] - explainer.n_skip_features
     assert np.round(np.sum(shap_value), 2) == np.round(explainer.fx - explainer.base_val, 2)
+
+
+def test_shap_values_multiple_instances(
+    boston_data: Tuple[np.ndarray, np.ndarray, np.ndarray, "sklearn.svm.SVR"]
+) -> None:
+    X_train, X_test, y_train, model = boston_data
+    explainer = shappack.KernelExplainer(model.predict, X_train[:DATA_SIZE])
+    n_data = 3
+    shap_value = explainer.shap_values(X_test[:n_data])
+    assert shap_value.shape[0] == n_data
+    assert shap_value.shape[1] == X_train.shape[1]
